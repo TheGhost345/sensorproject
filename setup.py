1d60fbfcdfcc110ac2bf9687bba65816b8eb5376
Kanha <padmalochanroutray3@gmail.com>
2025-07-19 09:26:34 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+    return requirements
+
+setup(
+    name='Fault Detection',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    author='Kanha',
+    author_email='padmalochanroutray3@gmail.com',
+)
